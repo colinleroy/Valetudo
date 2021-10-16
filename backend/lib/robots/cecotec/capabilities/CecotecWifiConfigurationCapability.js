05f547fdd80e1f750784bd5b6b3d41b6e5127820
Colin Leroy-Mira <colin.leroy-mira@sigfox.com>
2021-10-16 12:39:49 +0200

    Implement Cecotec setWifiConfiguration()

--- backend/lib/robots/cecotec/capabilities/CecotecWifiConfigurationCapability.js
+++ backend/lib/robots/cecotec/capabilities/CecotecWifiConfigurationCapability.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const spawnSync = require("child_process").spawnSync;
+
 const LinuxWifiConfigurationCapability = require("../../common/linuxCapabilities/LinuxWifiConfigurationCapability");
 const ValetudoWifiConfiguration = require("../../../entities/core/ValetudoWifiConfiguration");
 
@@ -21,10 +24,41 @@ class CecotecWifiConfigurationCapability extends LinuxWifiConfigurationCapabilit
     }
 
     /**
+     * @param {import("../../../entities/core/ValetudoWifiConfiguration")} wifiConfig
      * @returns {Promise<void>}
      */
-    async setWifiConfiguration() {
-        throw new Error("Cannot set Wi-Fi configuration for Cecotec vacuums");
+    async setWifiConfiguration(wifiConfig) {
+        if (this.robot.config.get("embedded") !== true) {
+            throw new Error("Cannot remotely set Wi-Fi configuration for Cecotec vacuums");
+        }
+        if (
+            wifiConfig && wifiConfig.ssid && wifiConfig.credentials &&
+            wifiConfig.credentials.type === ValetudoWifiConfiguration.CREDENTIALS_TYPE.WPA2_PSK &&
+            wifiConfig.credentials.typeSpecificSettings && wifiConfig.credentials.typeSpecificSettings.password
+        ) {
+
+            const wpa_supplicant_path = "/etc/wifi/wpa_supplicant.conf";
+            const wpa_config = "ctrl_interface=/etc/wifi/sockets\n"
+                    + "update_config=1\n"
+                    + "\n"
+                    + "network={\n"
+                    + "	ssid=\"" + wifiConfig.ssid + "\"\n"
+                    + "	scan_ssid=1\n"
+                    + "	psk=\"" + wifiConfig.credentials.typeSpecificSettings.password + "\"\n"
+                    + "	key_mgmt=WPA-PSK\n"
+                    + "	priority=7\n"
+                    + "}\n";
+
+            fs.writeFileSync(wpa_supplicant_path, wpa_config);
+
+            const wifi_status = spawnSync("/etc/init.d/robotWifiManager", ["restart"]);
+            if (wifi_status.status !== 0) {
+                throw new Error("Failed to setup wifi");
+            }
+        } else {
+            throw new Error("Unsupported wifi configuration");
+        }
+            
     }
 }
 
